ede6f9f06e74e49ad9140db80a5e7e8c02193b37
akhoroshkova <akhoroshkova@moysklad-team.ru>
2026-06-15 10:47:19 +0300

    init hw 1

---
+++ src/app/table/table.js
@@ -0,0 +1,7 @@
+import html from "./table.html";
+import './table.css'
+import './status-cell.css'
+
+
+const rootElement = document.getElementById('table-container');
+rootElement.innerHTML = html;
--- src/main.js
+++ src/main.js
@@ -2,3 +2,5 @@ import 'flowbite';
 import './style.css'
 
 import './app/app'
+import './app/table/table'
+import './app/add-item-modal/add-item-modal'
